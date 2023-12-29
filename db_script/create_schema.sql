ff3da524eb93f4e7a7b972be28f82578ac122d41
ahmed-aouinti <awintiahmed2017@gmail.com>
2023-12-29 10:02:14 +0100

    first commit

---
+++ db_script/create_schema.sql
@@ -0,0 +1,8 @@
+-- -----------------------------------------------------
+-- Schema cafe-management
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `cafe-management`;
+
+CREATE SCHEMA `cafe-management`;
+USE `cafe-management` ;
+
