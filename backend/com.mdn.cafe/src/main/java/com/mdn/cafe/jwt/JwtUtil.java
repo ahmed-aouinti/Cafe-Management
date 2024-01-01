048ab1e354253070f1089473b09918fa3866b3a5
ahmed-aouinti <awintiahmed2017@gmail.com>
2024-01-01 17:03:33 +0100

    created secret key

---
+++ backend/com.mdn.cafe/src/main/java/com/mdn/cafe/jwt/JwtUtil.java
@@ -0,0 +1,31 @@
+package com.mdn.cafe.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+
+    private String secret = "azerty";
+
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaims(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+}
