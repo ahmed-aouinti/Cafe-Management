1674f2129569c5d6ae4215e54dd3eb8b570dc8ed
ahmed-aouinti <awintiahmed2017@gmail.com>
2024-01-02 20:44:09 +0100

    validated and generated token

--- backend/com.mdn.cafe/src/main/java/com/mdn/cafe/jwt/JwtUtil.java
+++ backend/com.mdn.cafe/src/main/java/com/mdn/cafe/jwt/JwtUtil.java
@@ -2,9 +2,13 @@ package com.mdn.cafe.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -33,5 +37,25 @@ public class JwtUtil {
         return extractExpiration(token).before(new Date());
     }
 
- 
+    public String generateToken(String username, String role) {
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("role",role);
+        return createToken(claims,username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+
 }
